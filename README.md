6d03d770cab4d28966fb5ff2801bc4f54a53c5a4
DavidRico <davidrico.dev@gmail.com>
2024-06-19 12:50:32 +0200

    primer commmit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Jakarta listeners
+
+En este proyecto se recopilan todos los ejemplos de listeners con Jakarta en Java.
+
+## Contenidos
+
+Cada paquete representa una sección dentro del master:
+
+- Jakarta listeners:
+  - Introducción a los Listeners
+  - Guardando atributos de contextos
+  - **Tarea:** Listener nombre y apellido
---
+++ src/main/java/es/davidrico/jakarta/listeners/controladores/AgregarCarroServlet.java
@@ -0,0 +1,34 @@
+package es.davidrico.jakarta.listeners.controladores;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import es.davidrico.jakarta.listeners.modelos.Carro;
+import es.davidrico.jakarta.listeners.modelos.ItemCarro;
+import es.davidrico.jakarta.listeners.modelos.Producto;
+import es.davidrico.jakarta.listeners.servicios.ProductoService;
+import es.davidrico.jakarta.listeners.servicios.ProductoServiceImpl;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@WebServlet("/agregar-carro")
+public class AgregarCarroServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Long id = Long.parseLong(req.getParameter("id"));
+        ProductoService service = new ProductoServiceImpl();
+        Optional<Producto> producto = service.porId(id);
+        if (producto.isPresent()) {
+            ItemCarro item = new ItemCarro(1, producto.get());
+            HttpSession session = req.getSession();
+            Carro carro = (Carro) session.getAttribute("carro");
+            carro.addItemCarro(item);
+        }
+        resp.sendRedirect(req.getContextPath() + "/ver-carro");
+    }
+}
